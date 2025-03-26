ae295591778d63ee94b82f243d8087fff98a6a70
Sarif167 <wajsarif461@gmail.com>
2025-03-26 13:49:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg')
+START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/6JBnT0zB/58f7a8c73173.jpg https://i.ibb.co/6JBnT0zB/58f7a8c73173.jpg')).split()
+FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/6JBnT0zB/58f7a8c73173.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://i.ibb.co/cc3DV69V/4e8dfd2f1a59.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://i.ibb.co/S48z8FJr/90ec2f4e08e0.jpg')).split()
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
