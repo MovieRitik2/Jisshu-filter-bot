09fd62f5735257aa5967512d658e0b83f11fd0ec
Sarif167 <wajsarif461@gmail.com>
2025-03-29 12:28:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1249672673 7990174185').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Ritikmoves2") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002442237854'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002516837686'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/MovieSearchooooo')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 #---------------------------------------------------------------
